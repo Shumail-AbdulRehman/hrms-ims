3c817e20619217e914e3639945de942a61dc2c57
Asmatullah Atal <asmatullah6090@gmail.com>
2026-02-17 09:27:47 -0800

    test changes

---
+++ .history/backend/src/middlewares/authorize.middleware_20260217091856.js
@@ -0,0 +1,39 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import roles from "../config/roles.js";
+
+export const authorize = (module, action) =>
+    asyncHandler(async (req, res, next) => {
+        const user = req.user;
+
+        if (!user) {
+            throw new ApiError(401, "Unauthorized access");
+        }
+
+        const rolePermissions = roles[user.role];
+
+        if (!rolePermissions) {
+            throw new ApiError(
+                403,
+                "Role not found or no permissions assigned"
+            );
+        }
+
+        const modulePermissions = rolePermissions[module];
+
+        if (!modulePermissions || modulePermissions.length === 0) {
+            throw new ApiError(403, "Access denied to this module");
+        }
+
+        if (
+            !modulePermissions.includes("*") &&
+            !modulePermissions.includes(action)
+        ) {
+            throw new ApiError(
+                403,
+                "You do not have permission to perform this action  "
+            );
+        }
+
+        next();
+    });
---
+++ .history/backend/src/routes/personnel.route_20260217041503.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { signIn, signUp } from "../controllers/personnel.controller.js";
+
+const router = Router();
+
+router.route("/signIn").post(signIn);
+//this is he route for signing up a new user, it will be used by the admin to create new personnel accounts
+// the signUp function will be responsible for creating a new user in the database, it will take the user details from the request body and create a new user in the database, it will also hash the password before saving it to the database for security reasons
+router.route("/signUp").post(signUp);
+
+export default router;
