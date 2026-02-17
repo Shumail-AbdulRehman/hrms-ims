08891fbc0e083405b41fce4e9864005d294a0414
Shumail-AbdulRehman <47891@students.riphah.edu.pk>
2026-02-17 17:19:26 +0500

    testing 02

--- backend/src/routes/personnel.route.js
+++ backend/src/routes/personnel.route.js
@@ -4,8 +4,10 @@ import { signIn, signUp } from "../controllers/personnel.controller.js";
 const router = Router();
 
 router.route("/signIn").post(signIn);
-//this is he route for signing up a new user, it will be used by the admin to create new personnel accounts
-// the signUp function will be responsible for creating a new user in the database, it will take the user details from the request body and create a new user in the database, it will also hash the password before saving it to the database for security reasons
 router.route("/signUp").post(signUp);
+
+
+
+
 
 export default router;
